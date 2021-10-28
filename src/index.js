f2ae47f5bdcb953fe1ac956d991ad83b3a654ed1
Logan Butler <logan.butler@student.swic.edu>
2021-10-27 20:49:42 -0500

    Begin api and root for index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hiss");
+import api from "./services/api.service";
+
+api.index().then((data) => {
+  console.log(data);
+});
+
+const root = document.querySelector("#root");
+
+root.innerHTML = '<p class="text-center">"Hello World"</p>';
