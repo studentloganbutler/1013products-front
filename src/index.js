d61fa57543107f2bfb7a7e9021d78a30a2ef064e
Logan Butler <logan.butler@student.swic.edu>
2021-11-03 19:38:34 -0500

    Add mapping for product and price

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import api from "./services/api.service";
 
-api.index().then((data) => {
-  console.log(data);
-});
-
 const root = document.querySelector("#root");
 
+api.index().then((product) => {
+  root.innerHTML =
+    "<ul>" +
+    product
+      .map((product) => `<li>${product.name} ${product.price}<li>`)
+      .join("") +
+    "</ul>";
+});
+
 root.innerHTML = '<p class="text-center">"Hello World"</p>';
+
+// Note to self: if error is eslint, try CTRL S
